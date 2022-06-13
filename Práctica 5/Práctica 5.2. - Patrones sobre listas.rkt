bf9f39ca27be18235582c9464c13e2ff1ef3adf5
Martín Sferco <martinsferco@hotmail.com>
2022-06-13 16:00:35 -0300

    Ready to start Practice 5.2

---
+++ Práctica 5/Práctica 5.2. - Patrones sobre listas.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |Práctica 5.2. - Patrones sobre listas|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;__________________________________________
+;LICENCIATURA EN CIENCIAS DE LA COMPUTACIÓN
+;PROGRAMACIÓN I - 2022
+;PRÁCTICA 5 - Segunda parte - Patrones sobre listas
+
+;Martín Sferco
+;Comisión 1 - Grupo 7
+;__________________________________________
+;Ejercicio 1)
